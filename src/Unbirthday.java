60aec4a19a6af1c011a097798480229f59226c1c
Garrett-Thomas <league@WTS5.attlocal.net>
2017-11-13 16:56:28 -0800

    unbirthday,Remarcable IP

---
+++ src/Unbirthday.java
@@ -0,0 +1,17 @@
+import javax.swing.JOptionPane;
+
+public class Unbirthday {
+	public static void main(String[] args) {
+		String birthday = JOptionPane.showInputDialog(null, "What is your birthday");
+		if (birthday.equals("11/13")) {
+			JOptionPane.showMessageDialog(null, "Happy Birthday ");
+
+		}
+
+		else {
+
+			JOptionPane.showMessageDialog(null, "Happy Unbirthday");
+		}
+
+	}
+}
