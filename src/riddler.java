bf8d7f813722fbc2d0d75fde91317f0b165dbf3a
not you <league@WTS5.attlocal.net>
2017-12-13 16:55:45 -0800

    riddler IP

---
+++ src/riddler.java
@@ -0,0 +1,19 @@
+import javax.swing.JOptionPane;
+
+public class riddler {
+	public static void main(String[] args) {
+		int score = 0;
+		String riddle = JOptionPane.showInputDialog(
+				"What is greater than God, more evil than the devil, the poor have it, the rich need it, and if you eat it, you'll die?");
+		if (riddle.equals("nothing")) {
+			JOptionPane.showMessageDialog(null, "correct");
+			score++;
+			JOptionPane.showMessageDialog(null, "Your score is now " + score);
+
+		} else {
+			JOptionPane.showMessageDialog(null, "wrong, the correct answer is actually nothing");
+		}
+	}}{
+
+	String riddle = JOptionPane.showInputDialog(
+}"A doctor and a bus driver are both in love with the same woman, an attractive girl named Sarah. The bus driver had to go on a long bustrip that would last a week. Before he left, he gave Sarah seven apples. Why?");if(riddle.equals("An apple a day, keeps the doctors away! "));
