436bc7c085d6dcf6ff648d60e803dd25b05a4f49
kamronraiszadeh <league@WTS10.attlocal.net>
2018-01-17 15:55:34 -0800

    Badger

--- src/Badger.java
+++ src/Badger.java
@@ -1,9 +1,18 @@
 import java.awt.print.Printable;
 
 public class Badger {
-public static void main(String[] args) {
-	for (int i = 0; i < 10; i++) {
-		System.out.println("badger");
+	public static void main(String[] args) {
+
+		for (int s = 0; s < 10; s++) {
+			for (int i = 0; i < 10; i++) {
+				System.out.print("badger ");
+
+			}
+			for (int i = 0; i < 2; i++) {
+				System.out.print("mushroom ");
+			}
+			System.out.println();
+		}
 	}
 }
-}
+
